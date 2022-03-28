65fa5f928d2a46b2420a7d5f00f6c2c71ee6cf82
Julian-Villeneuve <julian.st@hotmail.fr>
2022-03-29 01:12:00 +0200

    update mesh

--- BezierCurveProject/openglwidget.cpp
+++ BezierCurveProject/openglwidget.cpp
@@ -1,11 +1,47 @@
 #include "openglwidget.h"
 
+int windowWidth = 500, windowHeight = 500;
+
+Vertex vertices[] = { Vertex(glm::vec3(-0.5,-0.5,0)),
+						  Vertex(glm::vec3(0,0.5,0)),
+						  Vertex(glm::vec3(0.5,-0.5,0))
+};
+
 OpenGLWidget::OpenGLWidget(QWidget *parent)
-	: QWidget(parent)
+	: QOpenGLWidget(parent)
 {
-	ui.setupUi(this);
+	_width = windowWidth;
+	_height = windowHeight;
+
 }
 
 OpenGLWidget::~OpenGLWidget()
 {
+}
+
+void OpenGLWidget::initializeGL()
+{
+	if (!initializeOpenGLFunctions()) {
+		QMessageBox::critical(this, "OpenGL initialization error", "MyOpenGLWidget::initializeGL() : Unable to initialize OpenGL functions");
+		exit(1);
+	}
+
+	_mesh = new Mesh(vertices, sizeof(vertices) / sizeof(vertices[0]));
+	_shader = new Shader("basicShader");
+}
+
+void OpenGLWidget::paintGL()
+{
+	glClearColor(1.0f, .95f, .75f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	_shader->Bind();
+	_mesh->Draw();
+	glFinish();
+}
+
+void OpenGLWidget::resizeGL(int width, int height)
+{
+	_width = width;
+	_height = height;
+	glViewport(0, 0, _width, _height);
 }
--- BezierCurveProject/openglwidget.h
+++ BezierCurveProject/openglwidget.h
@@ -1,16 +1,28 @@
 #pragma once
 
-#include <QWidget>
-#include "ui_openglwidget.h"
+#include <QOpenGLWidget>
+#include <QMessageBox>
+#include <QOpenGLFunctions>
+#include <QOpenGLFunctions_4_4_Core>
+#include "shader.h"
+#include "mesh.h"
 
-class OpenGLWidget : public QWidget
+class OpenGLWidget : public QOpenGLWidget, protected QOpenGLFunctions_4_4_Core
 {
 	Q_OBJECT
 
 public:
 	OpenGLWidget(QWidget *parent = Q_NULLPTR);
 	~OpenGLWidget();
-
+protected:
+	// OpenGL management
+	// -----------------
+	void initializeGL() override;
+	void paintGL() override;
+	void resizeGL(int width, int height) override;
 private:
-	Ui::OpenGLWidget ui;
+	int _width;
+	int _height;
+	Shader* _shader;
+	Mesh* _mesh;
 };
